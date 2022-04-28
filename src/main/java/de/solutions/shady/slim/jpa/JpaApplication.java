01b33956bd2c504b2ba7e851218e73d51fed171c
umschulungacademy <simon.schaad@online.de>
2022-04-28 14:53:22 +0200

    v.1.0.9 update

--- src/main/java/de/solutions/shady/slim/jpa/JpaApplication.java
+++ src/main/java/de/solutions/shady/slim/jpa/JpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -16,5 +18,10 @@ public class JpaApplication {
     @Bean
     public UserDetailsService userDetailsService() {
         return new MyUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/de/solutions/shady/slim/jpa/conf/MyUserDetailsService.java
+++ src/main/java/de/solutions/shady/slim/jpa/conf/MyUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -19,21 +20,33 @@ public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.debug("loadUserByUserName: {}", username);
 
         Optional<User> user = userRepository.findByUsernameIs((username));
         log.debug("user optional from database: {}", user);
+        log.debug("Simon: {}", passwordEncoder.encode("simon"));
 
         if (user.isPresent()) {
-            return org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder()
-                    .username(user.get().getUsername())
-                    .password(user.get().getPasswordEncoded()) //TODO: !!!
+            return org.springframework.security.core.userdetails.User.withUsername(user.get().getUsername())
+                    .password(user.get().getPasswordEncoded())
                     .roles("USER")
                     .build();
         } else {
             throw new UsernameNotFoundException("username " + username + " not found");
         }
+
+        /*return userRepository.findByUsernameIs(username)
+                .map(u -> org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder()
+                .username(u.getUsername())
+                        .password(u.getPasswordEncoded())
+                        .roles("USER")
+                        .build()
+                        .orElseThrow;
+        */
     }
 }
